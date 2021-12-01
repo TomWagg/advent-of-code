a3090933eb70c55f7c630bc1af0389a3541d432b
Tom Wagg <twagg@college.harvard.edu>
2021-12-01 13:17:33 -0800

    add comments, move inputs file

--- 2021/python/1.py
+++ 2021/python/1.py
@@ -1,9 +1,16 @@
-with open("inputs/1.txt") as input:
-    depths = list(map(int, input.readlines()))
+def main():
+    # get the depths from the file (mapped to integers)
+    with open("../inputs/1.txt") as input:
+        depths = list(map(int, input.readlines()))
 
-larger = 0
+    # count how many times the depth gets larger
+    larger = 0
+    for i in range(1, len(depths)):
+        larger += int(depths[i] - depths[i - 1] > 0)
 
-for i in range(1, len(depths)):
-    larger += int(depths[i] - depths[i - 1] > 0)
+    # print the result
+    print(larger)
 
-print(larger)
+
+if __name__ == "__main__":
+    main()
