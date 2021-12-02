0bd5e8bb8900c4696fc23c6352d522da31182612
Tom Wagg <twagg@college.harvard.edu>
2021-12-01 22:37:00 -0800

    day two, part two: python

--- 2021/python/2.py
+++ 2021/python/2.py
@@ -1,18 +1,43 @@
-def main():
+def move_ship(file):
     pos, depth = 0, 0
-    with open("../inputs/2.txt") as input:
+    with open(file) as input:
         for line in input:
             direction, magnitude = line.split()
+            mag_int = int(magnitude)
             if direction == "forward":
-                pos += int(magnitude)
+                pos += mag_int
             elif direction == "down":
-                depth += int(magnitude)
+                depth += mag_int
             elif direction == "up":
-                depth -= int(magnitude)
+                depth -= mag_int
             else:
                 raise ValueError()
 
-    print(pos * depth)
+    return pos * depth
+
+
+def move_ship_with_aim(file):
+    pos, depth, aim = 0, 0, 0
+    with open(file) as input:
+        for line in input:
+            direction, magnitude = line.split()
+            mag_int = int(magnitude)
+            if direction == "forward":
+                pos += mag_int
+                depth += mag_int * aim
+            elif direction == "down":
+                aim += mag_int
+            elif direction == "up":
+                aim -= mag_int
+            else:
+                raise ValueError()
+
+    return pos * depth
+
+
+def main():
+    print("PART ONE:", move_ship("../inputs/2.txt"))
+    print("PART TWO:", move_ship_with_aim("../inputs/2.txt"))
 
 
 if __name__ == "__main__":
