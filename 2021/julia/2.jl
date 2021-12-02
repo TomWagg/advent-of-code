d87c36bd0d2a01fbc734e58a29d5849ed7ceb4a8
Tom Wagg <twagg@college.harvard.edu>
2021-12-01 22:38:03 -0800

    day 2 part 2: julia

--- 2021/julia/2.jl
+++ 2021/julia/2.jl
@@ -18,8 +18,30 @@ function move_ship(file)
     return pos * depth
 end;
 
+function move_ship_with_aim(file)
+    pos, depth, aim = 0, 0, 0
+    open("../inputs/2.txt", "r") do input
+        for line in eachline(input)
+            direction, magnitude = split(line)
+            mag_int = parse.(Int, magnitude)
+            if direction == "forward"
+                pos += mag_int
+                depth += mag_int * aim
+            elseif direction == "down"
+                aim += mag_int
+            elseif direction == "up"
+                aim -= mag_int
+            else
+                error("Invalid direction")
+            end;
+        end;
+    end;
+    return pos * depth
+end;
+
 function main()
-    println(move_ship("../inputs/2.txt"))
+    println("PART ONE: ", move_ship("../inputs/2.txt"))
+    println("PART TWO: ", move_ship_with_aim("../inputs/2.txt"))
 end;
 
 main()
