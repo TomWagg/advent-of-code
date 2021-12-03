6b7426d2001d937c2dbc023bc2c1f33c97939cc6
Tom Wagg <twagg@college.harvard.edu>
2021-12-03 10:37:58 -0800

    better comments and factoring

--- 2021/python/3.py
+++ 2021/python/3.py
@@ -7,6 +7,7 @@ def binary_to_decimal(binary):
 
 
 def get_most_common(diagnostics, digit):
+    """ Get the most common digit over an array of `diagnostics` at index `digit` """
     total = 0
     for i in range(len(diagnostics)):
         total += int(diagnostics[i][digit])
@@ -15,29 +16,29 @@ def get_most_common(diagnostics, digit):
     return 0 if total < 0.5 else 1
 
 
-def main():
-    # read in diagnostics
-    with open("../inputs/3.txt") as input:
-        diagnostics = [line.replace("\n", "") for line in input]
-
+def get_power_consumption(diagnostics):
     # count how many digits in each binary number
     n_digit = len(diagnostics[0])
 
     most_common = ""
     least_common = ""
 
-    # loop over each digit
+    # get most and least common at each digit
     for i in range(n_digit):
-        # this gives most common and least common
         most = get_most_common(diagnostics, i)
         most_common += str(most)
         least_common += str(1 - most)
 
     # convert numbers to decimals and multiply
-    power = binary_to_decimal(most_common) * binary_to_decimal(least_common)
+    return binary_to_decimal(most_common) * binary_to_decimal(least_common)
 
-    # part 1 done!
-    print(power)
+
+def main():
+    # read in diagnostics
+    with open("../inputs/3.txt") as input:
+        diagnostics = [line.replace("\n", "") for line in input]
+
+    print("PART ONE:", get_power_consumption(diagnostics))
 
 
 if __name__ == "__main__":
