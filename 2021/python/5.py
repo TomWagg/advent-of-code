e7c3e1ddf595236b6e33b8a359edaba723fa4c4a
Tom Wagg <twagg@college.harvard.edu>
2021-12-05 18:11:34 -0800

    day 5 part 2 (python)

--- 2021/python/5.py
+++ 2021/python/5.py
@@ -7,22 +7,32 @@ def count_overlaps(lines, max_x, max_y):
         x_i, y_i = start
         x_f, y_f = end
 
+        # slightly wasteful and do everything for x and y
+        # work out offset and step size (depending on direction)
+        x_offset = -1 if x_f < x_i else 1
+        x_step = -1 if x_f < x_i else 1
+        x_range = range(x_i, x_f + x_offset, x_step)
+
+        y_offset = -1 if y_f < y_i else 1
+        y_step = -1 if y_f < y_i else 1
+        y_range = range(y_i, y_f + y_offset, y_step)
+
         # if horizontal line
         if x_i == x_f:
-            # work out offset and step size (depending on direction)
-            offset = -1 if y_f < y_i else 1
-            step = -1 if y_f < y_i else 1
-
-            for y in range(y_i, y_f + offset, step):
+            for y in y_range:
                 grid[x_i][y] += 1
 
         # if vertical line
         elif y_i == y_f:
             # work out offset and step size (depending on direction)
-            offset = -1 if x_f < x_i else 1
-            step = -1 if x_f < x_i else 1
-            for x in range(x_i, x_f + offset, step):
+            for x in x_range:
                 grid[x][y_i] += 1
+
+        # if diagonal line
+        else:
+            for x, y in zip(x_range, y_range):
+                grid[x][y] += 1
+
 
     # sum up the number of points that have at least 2 overlaps
     overlaps = sum([int(item >= 2) for row in grid for item in row])
@@ -40,7 +50,8 @@ def main():
             max_y = max(max_y, max(start[1], end[1]))
             lines.append([start, end])
 
-    print("PART ONE:", count_overlaps(lines, max_x, max_y))
+    print("FULL SOLUTION:", count_overlaps(lines, max_x, max_y))
+
 
 if __name__ == "__main__":
     main()
