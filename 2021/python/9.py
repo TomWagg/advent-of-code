83a98cf6e1aef7c2cf4ab805a79945fb82844b9a
Tom Wagg <twagg@college.harvard.edu>
2021-12-13 12:52:48 -0500

    day 9 python

---
+++ 2021/python/9.py
@@ -0,0 +1,65 @@
+def find_minima(heights, n_col):
+    minima = []
+    for i in range(len(heights)):
+        min_nearest = None
+        for index in [i - 1, i + 1, i - n_col, i + n_col]:
+            if index > 0 and index < len(heights):
+                if min_nearest is None or heights[index] < min_nearest:
+                    min_nearest = heights[index]
+        if heights[i] < min_nearest:
+            minima.append(i)
+    return minima
+
+
+def find_basin_sizes(heights, minima_inds, n_col):
+    basin_sizes = [len(grow_basin(heights,
+                                  ind,
+                                  members=[],
+                                  n_col=n_col))
+                   for ind in minima_inds]
+    return basin_sizes
+
+
+def grow_basin(heights, position, members, n_col):
+    # base case: we hit a 9 or already added this position
+    if heights[position] == 9 or position in members:
+        return members
+    else:
+        members.append(position)
+
+        directions = []
+        if position - n_col >= 0:
+            directions.append(-n_col)
+        if position + n_col < len(heights):
+            directions.append(n_col)
+        if position % n_col != 0:
+            directions.append(-1)
+        if (position + 1) % n_col != 0:
+            directions.append(1)
+
+        for direction in directions:
+            members = grow_basin(heights, position + direction, members, n_col)
+        return members
+
+
+def main():
+    heights = []
+    n_col = 0
+    with open("../inputs/9.txt") as input:
+        for line in input:
+            n_col = len(line.strip())
+            heights.extend(list(map(int, list(line.strip()))))
+
+    minima_inds = find_minima(heights, n_col)
+
+    print("PART ONE:", sum([int(heights[i]) + 1 for i in minima_inds]))
+
+    basin_sizes = find_basin_sizes(heights, minima_inds, n_col)
+    top_three = sorted(basin_sizes)[-3:]
+    total = top_three[0] * top_three[1] * top_three[2]
+
+    print("PART TWO:", total)
+
+
+if __name__ == "__main__":
+    main()
