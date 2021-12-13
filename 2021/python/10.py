8405c1916c7fb69a602883aedbc7b10a6d799c85
Tom Wagg <twagg@college.harvard.edu>
2021-12-13 15:03:03 -0500

    day 10 part 2 (python)

--- 2021/python/10.py
+++ 2021/python/10.py
@@ -1,29 +1,42 @@
 def main():
     openers = ["(", "[", "{", "<"]
     closers = [")", "]", "}", ">"]
-    matches = {")": ("(", 3, 1),
-               "]": ("[", 57, 2),
-               "}": ("{", 1197, 3),
-               ">": ("<", 25137, 4)}
+    close_to_open = {")": ("(", 3),
+                     "]": ("[", 57),
+                     "}": ("{", 1197),
+                     ">": ("<", 25137)}
+    open_to_close = {"(": (")", 1),
+                     "[": ("]", 2),
+                     "{": ("}", 3),
+                     "<": (">", 4)}
 
     with open("../inputs/10.txt") as input:
         total_corruption = 0
+        incomplete_totals = []
         for line in input:
             queue = []
+            corrupt = False
             for character in line.strip():
                 if character in openers:
                     queue.append(character)
                 if character in closers:
                     actual = queue.pop()
-                    expected, value, _ = matches[character]
+                    expected, value = close_to_open[character]
                     if expected != actual:
                         total_corruption += value
+                        corrupt = True
                         break
 
-            if queue != []:
-                print("INCOMLETE LINE")
+            if not corrupt:
+                total_incomplete = 0
+                while queue != []:
+                    required, value = open_to_close[queue.pop()]
+                    total_incomplete *= 5
+                    total_incomplete += value
+                incomplete_totals.append(total_incomplete)
 
-    print(total_corruption)
+    print("PART ONE:", total_corruption)
+    print("PART TWO:", sorted(incomplete_totals)[(len(incomplete_totals) - 1) // 2])
 
 
 if __name__ == "__main__":
