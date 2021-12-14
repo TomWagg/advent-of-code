0e2abee12a6b6705cb6379cbfa19fa7efe144463
Tom Wagg <twagg@college.harvard.edu>
2021-12-13 23:44:57 -0500

    day 12 part 2 (python)

--- 2021/python/12.py
+++ 2021/python/12.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 
+
 def build_graph(pairs):
     # build up a dictionary of edges
     graph = defaultdict(list)
@@ -24,6 +25,25 @@ def find_paths_to_end(graph, start="start", current_path=["start"], paths=0):
     return paths
 
 
+def find_paths_to_end_part_two(graph, start="start", current_path=["start"], double_visit=False, paths=0):
+    # go through every node attached to this one
+    for node in graph[start]:
+        # special case for the end of the line
+        if node == "end":
+            paths += 1
+        # as long as we haven't visited the cave when it is small then continue searching
+        elif not (node in current_path and node.islower()):
+            paths = find_paths_to_end_part_two(graph, start=node, current_path=current_path + [node],
+                                               double_visit=double_visit, paths=paths)
+        # ignore unless we haven't visited one twice yet
+        elif not double_visit and node != "start":
+            paths = find_paths_to_end_part_two(graph, start=node, current_path=current_path + [node],
+                                               double_visit=True, paths=paths)
+
+    # return the total
+    return paths
+
+
 def main():
     pairs = []
     with open("../inputs/12.txt") as input:
@@ -33,6 +53,9 @@ def main():
     graph = build_graph(pairs)
     paths = find_paths_to_end(graph)
     print("PART ONE:", paths)
+
+    paths = find_paths_to_end_part_two(graph)
+    print("PART TWO:", paths)
 
 
 if __name__ == "__main__":
