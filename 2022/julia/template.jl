5016b5a24adf112a2d7f63363491e5c130f83cff
Tom Wagg <tomjwagg@gmail.com>
2022-12-13 22:31:58 -0800

    add a template to speed things up

---
+++ 2022/julia/template.jl
@@ -0,0 +1,14 @@
+function part_one()
+    open("../inputs/.txt", "r") do input
+        for line in eachline(input)
+        end
+    end
+    return nothing
+end
+
+function part_two()
+    return nothing
+end
+
+println("PART ONE: ", part_one())
+println("PART TWO: ", part_two())
