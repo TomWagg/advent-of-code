a17c2571da7aa14b54ce117ac651bc3c366f2248
Tom Wagg <tomjwagg@gmail.com>
2022-12-15 12:55:18 -0800

    optimise and comment day 15 2022, this one hurt

--- 2022/julia/15.jl
+++ 2022/julia/15.jl
@@ -1,12 +1,11 @@
-using DataStructures
+using BenchmarkTools
 
 function manhattan_distance(a::Array{Int}, b::Array{Int})
     return abs(a[1] - b[1]) + abs(a[2] - b[2])
 end
 
-function get_sensors_beacons()
+function get_sensors()
     sensors = Array{Int64}[]
-    beacons = Set()
     open("../inputs/15.txt", "r") do input
         for line in eachline(input)
             sensor_string, beacon_string = split(line, ":")
@@ -18,48 +17,71 @@ function get_sensors_beacons()
             beacon = [beacon_x, beacon_y]
             radius = manhattan_distance([sensor_x, sensor_y], beacon)
 
+            # store each sensor as (x, y, radius_of_influence)
             push!(sensors, [sensor_x, sensor_y, radius])
-            push!(beacons, beacon)
         end
     end
-    return sensors, beacons
+    return sensors
 end
 
 function part_one()
-    sensors, beacons = get_sensors_beacons()
+    sensors = get_sensors()
 
-    ROW_HEIGHT = 2000000
-    no_beacons = Set()
-    for i in 1:length(sensors)
-        extra_radius = abs(sensors[i][2] - ROW_HEIGHT) - sensors[i][3]
-        if extra_radius <= 0
-            push!(no_beacons, [sensors[i][1], ROW_HEIGHT])
-            for j in 1:abs(extra_radius)
-                push!(no_beacons, [sensors[i][1] - j, ROW_HEIGHT])
-                push!(no_beacons, [sensors[i][1] + j, ROW_HEIGHT])
-            end
+    ROW_HEIGHT = 2_000_000
+    covered_ranges = Array{Int64}[]
+
+    # go over each sensor and check how much of the row it covers
+    for i in eachindex(sensors)
+        # first calculate how much extra distance it covers after reaching the row
+        extra_radius = sensors[i][3] - abs(sensors[i][2] - ROW_HEIGHT)
+
+        # if it reaches the row, add a range of x values covered
+        if extra_radius >= 0
+            push!(covered_ranges, [sensors[i][1] - extra_radius, sensors[i][1] + extra_radius])
         end
     end
-    return length(setdiff(no_beacons, beacons))
+    
+    # sort the ranges by start time
+    sort!(covered_ranges, by=x->x[1])
+
+    # merge ranges into a series of disjoint ranges
+    stack = [covered_ranges[1]]
+    for rng in covered_ranges
+        if rng[1] > last(stack)[2]
+            push!(stack, rng)
+        elseif rng[2] > last(stack)[2]
+            last(stack)[2] = rng[2]
+        end
+    end
+
+    # count up the length of each disjoint range and return the total
+    total_covered = 0
+    for i in eachindex(stack)
+        total_covered += (stack[i][2] - stack[i][1])
+    end
+    return total_covered
 end
 
-function rotate(x, y)
+function rotate(x::Int64, y::Int64)
+    # rotate coordinates by 45 degrees
     return -x + y, x + y
 end
 
-function anti_rotate(x, y)
+function anti_rotate(x::Int64, y::Int64)
+    # undo the rotation of the function above
     return [y - ((x + y) ÷ 2), (x + y) ÷ 2]
 end
 
-function get_rotated_corners(sensor)
-    #lrtb
-    return Dict("bottom left" => rotate(sensor[1], sensor[2] - sensor[3]),      # left bottom
-                "top right" => rotate(sensor[1], sensor[2] + sensor[3]),      # right top
-                "top left" => rotate(sensor[1] + sensor[3], sensor[2]),      # left top
-                "bottom right" => rotate(sensor[1] - sensor[3], sensor[2]))      # right bottom
+function get_rotated_corners(sensor::Array{Int64})
+    # rotate all of the corners of influence of a sensor
+    return Dict("bottom left" => rotate(sensor[1], sensor[2] - sensor[3]),
+                "top right" => rotate(sensor[1], sensor[2] + sensor[3]),
+                "top left" => rotate(sensor[1] + sensor[3], sensor[2]),
+                "bottom right" => rotate(sensor[1] - sensor[3], sensor[2]))
 end
 
-function common_range(a1, a2, b1, b2)
+function common_range(a1::Int64, a2::Int64, b1::Int64, b2::Int64)
+    # work out the common range between two ranges
     lower = max(a1, b1)
     upper = min(a2, b2)
 
@@ -70,15 +92,20 @@ function common_range(a1, a2, b1, b2)
     end
 end
 
-function compare_sensors(s1, s2)
+function compare_sensors(s1::Array{Int64}, s2::Array{Int64})
+    # we're rotating them 45 degrees to get squares rather than diamonds because that make my head hurt less
     c1 = get_rotated_corners(s1)
     c2 = get_rotated_corners(s2)
+
+    # compare edges to other boxes
+    # the missing beacon can only occur when the edges have a gap between them (difference is 2)
+    # so we basically just find the cases where this occurs and then brute force those ones
 
     # this top, other bottom
     if c1["top left"][2] + 2 == c2["bottom left"][2]
         overlap = common_range(c1["top left"][1], c1["top right"][1],
                                c2["bottom left"][1], c2["bottom right"][1])
-        if overlap != nothing
+        if overlap !== nothing
             return Set([anti_rotate(i, c1["top left"][2] + 1) for i in overlap[1]:overlap[2]])
         end
 
@@ -86,7 +113,7 @@ function compare_sensors(s1, s2)
     elseif c1["top right"][1] + 2 == c2["top left"][1]
         overlap = common_range(c1["bottom right"][2], c1["top right"][2],
                                c2["bottom left"][2], c2["bottom left"][2])
-        if overlap != nothing
+        if overlap !== nothing
             return Set([anti_rotate(c1["top right"][2] + 1, i) for i in overlap[1]:overlap[2]])
         end
 
@@ -94,7 +121,7 @@ function compare_sensors(s1, s2)
     elseif c1["bottom left"][1] - 2 == c2["top left"][1]
         overlap = common_range(c1["bottom left"][1], c1["bottm right"][1],
                                c2["top left"][1], c2["top right"][1])
-        if overlap != nothing
+        if overlap !== nothing
             return Set([anti_rotate(c1["bottom left"][2] - 1, i) for i in overlap[1]:overlap[2]])
         end
 
@@ -102,60 +129,67 @@ function compare_sensors(s1, s2)
     elseif c1["top left"][1] - 2 == c2["top right"][1]
         overlap = common_range(c1["bottom left"][2], c1["top left"][2],
                                c2["bottom right"][2], c2["bottom right"][2])
-        if overlap != nothing
+        if overlap !== nothing
             return Set([anti_rotate(c1["top left"][1] - 1, i) for i in overlap[1]:overlap[2]])
         end
     end
 end
 
-function is_the_beacon(sensors, coord)
-    if coord[1] < 0 || coord[2] > 4_000_000#20
+function is_the_beacon(sensors::Array{Array{Int64}}, coord::Array{Int64})
+    # check if a coordinate is the beacon we are looking for
+
+    # make sure it is in the valid range
+    if coord[1] < 0 || coord[2] > 4_000_000
         return false
     end
-    if all(isinteger.(coord))
-        coord = Int.(coord)
-    else
-        return false
-    end
+
+    # check if it is too close to any sensor
     for sensor in sensors
         if manhattan_distance(sensor[1:2], coord) <= sensor[3]
             return false
         end
     end
+
+    # if not then we found it!
     return true
 end
 
 function part_two()
-    sensors, beacons = get_sensors_beacons()
+    # get all of the sensors
+    sensors = get_sensors()
 
-    BOUNDARY = 4_000_000
-
-    # rings = collect(out_of_reach_rings(sensors, BOUNDARY))
-    # sort!(rings, by=x->x[2], rev=true)
-
+    # setup a set of candidate coordinates
     candidates = Set()
-    for i in 1:length(sensors)
-        for j in 1:length(sensors)
+
+    # loop over the sensors in pairs
+    for i in eachindex(sensors)
+        for j in eachindex(sensors)
+            # ignore self comparison
             if i != j
-                new_things = compare_sensors(sensors[i], sensors[j])
-                if new_things != nothing
-                    union!(candidates, new_things)
+                # get a list of new candidates
+                new_candidates = compare_sensors(sensors[i], sensors[j])
+
+                # if we found any then combine them with the current set
+                if new_candidates !== nothing
+                    union!(candidates, new_candidates)
                 end
             end
         end
     end
 
-    # @show candidates
+    # check whether every candidate is the beacon and return if you find it
     for coord in candidates
         if is_the_beacon(sensors, coord)
-            return coord[1] * BOUNDARY + coord[2]
+            return coord[1] * 4_000_000 + coord[2]
         end
     end
+
+    # shouldn't reach here :crosses-fingers:
     return nothing
 end
 
-# @show Set([[i, i + 1] for i in 1:5])
-
-# println("PART ONE: ", part_one())
+println("PART ONE: ", part_one())
 println("PART TWO: ", part_two())
-# @time part_two()
+
+@btime part_one()
+@btime part_two()
