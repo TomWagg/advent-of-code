ca6faac55120b29564029d0ab1caa4364ef0ef7e
Tom Wagg <tomjwagg@gmail.com>
2022-12-16 13:00:01 -0800

    commented part one

--- 2022/julia/16.jl
+++ 2022/julia/16.jl
@@ -34,7 +34,7 @@ function construct_graph()
         end
     end
 
-    # now we can reduce this graph by deleting 
+    # now we can reduce this graph by deleting edges for broken valves and re-weighting the distances
     delete_these = []
     for i in 1:n
         if g.flow_rate[i] == 0 && label_to_ind["AA"] !== i
@@ -57,19 +57,22 @@ function construct_graph()
         end
     end
 
+    # this still leaves you with a big graph, so now convert to a smaller one (our case is 15 nodes + start)
     n = sum(g.flow_rate .> 0) + 1
 
+    # create a convertor from old inds to new ones
     start_ind = label_to_ind["AA"]
-    convertor = Dict(start_ind=>1)
-
+    convertor = Dict(start_ind => 1)
     old_inds = findall(x->x>0, g.flow_rate)
     for i in 2:n
         convertor[old_inds[i - 1]] = i
     end
 
+    # start a new smaller graph
     small_g = ValveGraph(range(1, n), [Set{Int8}() for i in 1:n], zeros(Int8, n, n), zeros(Int8, n))
     new_start_ind = Int8(-1)
 
+    # transfer over the old graph
     i = Int8(1)
     for v in g.v
         if g.flow_rate[v] > 0 || v == start_ind
@@ -90,38 +93,52 @@ function construct_graph()
 end
 
 function part_one(g::ValveGraph, start_ind::Int8)
+    """This performs an interactive cached search of the graph, searching for best order in which to open
+    valves and pre-calculates how much pressure to release"""
+
+    # start off an initial visited bool array
     initial_visited = falses(length(g.v))
     initial_visited[start_ind] = true
 
-    volcano_states = [(30, start_ind, 0, copy(initial_visited))]
-    state_tracker = DefaultDict(-1)
+    # start a queue of volcano states
+    volcano_states = [(30, start_ind, 0, initial_visited)]
 
+    # keep a tracker on the pressure released by each state (this is the cache)
+    state_tracker = DefaultDict(-1)
     maximum_pressure = 0
 
+    # iterate over the queue
     while length(volcano_states) > 0
+        # pop off the top of the queue
         time_left, location, pressure, visited = pop!(volcano_states)
-        
+
+        # if we've been here before and it wasn't as good then skip it
         if state_tracker[(time_left, location, visited)] >= pressure
             continue
+        # otherwise update the state tracker with this pressure
+        else
+            state_tracker[(time_left, location, visited)] = pressure
         end
-        state_tracker[(time_left, location, visited)] = pressure
 
-        if time_left < 0 || all(visited .== true)
+        # if this state has run out of time then (potentially) update the maximum pressure and move on
+        if time_left < 0
             maximum_pressure = max(maximum_pressure, pressure)
             continue
         end
 
+        # first option: open the valve!
         if !visited[location]
-            visited[location] = true
+            new_visited = copy(visited)
+            new_visited[location] = true
             push!(volcano_states, (time_left - 1, location,
                                    pressure + ((time_left - 1) * g.flow_rate[location]),
-                                   copy(visited)))
-            visited[location] = false
+                                   new_visited))
         end
 
+        # otherwise, try moving to each valve in the vicinity (adjust time by distance)
         for connection in collect(g.e[location])
             push!(volcano_states, (time_left - g.dist[location, connection], connection,
-                                   pressure, copy(visited)))
+                                   pressure, visited))
         end
     end
     return maximum_pressure
@@ -183,7 +200,7 @@ end
 function main()
     g, start_ind = construct_graph()
     println("PART ONE: ", part_one(g, Int8(start_ind)))
-    println("PART TWO: ", part_two(g, Int8(start_ind)))
+    # println("PART TWO: ", part_two(g, Int8(start_ind)))
 end
 
 main()
