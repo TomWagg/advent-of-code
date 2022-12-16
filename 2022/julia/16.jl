5f76f4868b1ff4ba8eaa13c22fa2f95833cc9957
Tom Wagg <tomjwagg@gmail.com>
2022-12-16 10:09:26 -0800

    okay graph set up now

--- 2022/julia/16.jl
+++ 2022/julia/16.jl
@@ -1,14 +1,23 @@
-using Graphs
+using Combinatorics
 
-# mutable struct ValveGraph
-#     v::Array{Int64}
-#     e::Matrix{Int64}
-#     w::Array{Int64}
-#     flow_rate::Array{Int64}
+mutable struct ValveGraph
+    v::Array{Int64}  # list of vertices
+    e  # list of vertices for each vertex implying an edge
+    dist::Matrix{Int64}  # distance along each edge
+    flow_rate::Array{Int64} # flow rate for each vertex
+    visited::Array{Bool}  # whether this valve has been visited
+end
+
+function construct_graph()
+    open("../inputs/16.txt", "r") do input
+        i = 1
+        for line in eachline(input)
+            label_to_ind[line[7:8]] = i
+            i += 1
+        end
+    end
     
-# end
-
-# function construct_graph
+end
 
 
 function part_one()
@@ -20,41 +29,70 @@ function part_one()
             i += 1
         end
     end
-    g = SimpleWeightedGraph(length(label_to_ind))
-    flow_rates = zeros(Int64, length(label_to_ind))
 
+    n = length(label_to_ind)
+    g = ValveGraph(range(1, n), [Set() for i in 1:n], zeros(Int64, n, n), zeros(Int64, n), falses(n))
     open("../inputs/16.txt", "r") do input
         i = 1
         for line in eachline(input)
-            flow_rates[i] = parse(Int, match(r"(?<=\=)\d+(?=;)", line).match)
+            g.flow_rate[i] = parse(Int, match(r"(?<=\=)\d+(?=;)", line).match)
             connections = match(r"(?<=(\bvalve \b)|(\bvalves )\b)[A-Z, ]*", line).match
             for connection in split(connections, ", ")
-                add_edge!(g, i, label_to_ind[connection])
+                push!(g.e[i], label_to_ind[connection])
+                push!(g.e[label_to_ind[connection]], i)
+                g.dist[i, label_to_ind[connection]] = 1
+                g.dist[label_to_ind[connection], i] = 1
             end
             i += 1
         end
     end
 
-    @show edges(g)
+    @show g
 
-    time_left = 30
-    curr_valve = label_to_ind["AA"]
-    total_pressure = 0
-
-    distances = floyd_warshall_shortest_paths(g)
-    @show distances.dists[1, :]
-
-    while time_left > 0
-        distances = dijkstra_shortest_paths(g, curr_valve).dists
-        @show distances
-        break
-        time_left_on_arrival = time_left .- distances .- 1
-        pressure_released = flow_rates .* time_left_on_arrival
-        # @show distances
-
-        # @show distances, time_left_on_arrival, pressure_released
-        time_left -= 1
+    delete_these = []
+    for i in 1:n
+        if g.flow_rate[i] == 0 && label_to_ind["AA"] !== i
+            push!(delete_these, i)
+            for pair in combinations(collect(g.e[i]), 2)
+                push!(g.e[pair[1]], pair[2])
+                push!(g.e[pair[2]], pair[1])
+                g.dist[pair[1], pair[2]] = g.dist[pair[1], i] + g.dist[pair[2], i]
+                g.dist[pair[2], pair[1]] = g.dist[pair[1], pair[2]]
+                
+                delete!(g.e[i], pair[1])
+                delete!(g.e[i], pair[2])
+                delete!(g.e[pair[1]], i)
+                delete!(g.e[pair[2]], i)
+                g.dist[i, pair[1]] = 0
+                g.dist[i, pair[2]] = 0
+                g.dist[pair[1], i] = 0
+                g.dist[pair[2], i] = 0
+            end
+        end
     end
+
+    deleteat!(g.v, delete_these)
+
+    @show g
+
+    # time_left = 30
+    # curr_valve = label_to_ind["AA"]
+    # total_pressure = 0
+
+    # distances = floyd_warshall_shortest_paths(g)
+    # @show distances.dists[1, :]
+
+    # while time_left > 0
+    #     distances = dijkstra_shortest_paths(g, curr_valve).dists
+    #     @show distances
+    #     break
+    #     time_left_on_arrival = time_left .- distances .- 1
+    #     pressure_released = flow_rates .* time_left_on_arrival
+    #     # @show distances
+
+    #     # @show distances, time_left_on_arrival, pressure_released
+    #     time_left -= 1
+    # end
 
     return nothing
 end
@@ -64,4 +102,4 @@ function part_two()
 end
 
 println("PART ONE: ", part_one())
-println("PART TWO: ", part_two())
+# println("PART TWO: ", part_two())
