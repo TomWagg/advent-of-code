2bee073a7578ff9260868f54da603884ef7f7e3c
Tom Wagg <tomjwagg@gmail.com>
2022-12-17 16:41:15 -0800

    day 17 2022 done finally, julia really screwed me here

--- 2022/julia/17.jl
+++ 2022/julia/17.jl
@@ -1,3 +1,5 @@
+using DataStructures
+
 function youve_got_wind()
     wind = nothing
     open("../inputs/17.txt", "r") do input
@@ -8,6 +10,41 @@ function youve_got_wind()
         end
     end
     return wind
+end
+
+function print_cave(blocked_positions::BitMatrix, limit::Int64)
+    println("+-------+")
+    for i in 1:limit - 1
+        print("|")
+        for j in 2:8
+            if blocked_positions[j, limit + 1 - i]
+                print("#")
+            else
+                print(".")
+            end
+        end
+        print("|")
+        println()
+    end
+    println("+-------+")
+end
+
+function cave_to_file(blocked_positions::BitMatrix, limit::Int64)
+    open("17.out", "w") do f
+        write(f, "+-------+\n")
+        for i in 1:limit - 1
+            write(f, "|")
+            for j in 2:8
+                if blocked_positions[j, limit + 1 - i]
+                    write(f, "#")
+                else
+                    write(f, ".")
+                end
+            end
+            write(f, "|\n")
+        end
+        write(f, "+-------+\n")
+    end
 end
 
 function shift_block_down(block::Vector{Vector{Int64}}, mag::Int64)
@@ -24,7 +61,7 @@ function shift_block_right(block::Vector{Vector{Int64}}, mag::Int64)
     return block
 end
 
-function can_move_down(blocked_positions, block)
+function can_move_down(blocked_positions::BitMatrix, block::Vector{Vector{Int64}})
     for i in eachindex(block)
         if blocked_positions[block[i][1], block[i][2] - 1]
             return false
@@ -33,7 +70,7 @@ function can_move_down(blocked_positions, block)
     return true
 end
 
-function can_move_right(blocked_positions, block)
+function can_move_right(blocked_positions::BitMatrix, block::Vector{Vector{Int64}})
     for i in eachindex(block)
         if blocked_positions[block[i][1] + 1, block[i][2]]
             return false
@@ -42,7 +79,7 @@ function can_move_right(blocked_positions, block)
     return true
 end
 
-function can_move_left(blocked_positions, block)
+function can_move_left(blocked_positions::BitMatrix, block::Vector{Vector{Int64}})
     for i in eachindex(block)
         if blocked_positions[block[i][1] - 1, block[i][2]]
             return false
@@ -51,7 +88,19 @@ function can_move_left(blocked_positions, block)
     return true
 end
 
-function part_one(n_block_max)
+function get_surface_heights(blocked_positions::BitMatrix, highest_block::Int64)
+    surface_heights = Array{Int64}(undef, 7)
+    for x in 2:8
+        y = highest_block
+        while !blocked_positions[x, y]
+            y -= 1
+        end
+        surface_heights[x - 1] = y - highest_block
+    end
+    return surface_heights
+end
+
+function drop_rocks(n_block_max::Int64)
     wind = youve_got_wind()
 
     blocks = []
@@ -66,36 +115,49 @@ function part_one(n_block_max)
     highest_block = 1
     total_highest_block = 0
 
-    blocked_positions = falses(9, 500)
+    blocked_positions = falses(9, 14 * 50000)
     blocked_positions[1, :] .= 1
     blocked_positions[9, :] .= 1
     blocked_positions[:, 1] .= 1
 
+    cache = Dict{Tuple, Tuple}()
+    heights = []
+
     n_blocks = 1
     wind_cursor = 1
     while n_blocks <= n_block_max
+        push!(heights, highest_block)
         current_block = deepcopy(blocks[((n_blocks - 1) % nb) + 1])
-        # @show current_block
-        bottom_of_block = minimum(x->x[2], current_block)
-        # left_of_block = minimum(x->x[1], current_block)
-        # right_of_block = maximum(x->x[1], current_block)
+        bottom_of_block = minimum(x -> x[2], current_block)
+
+        key = get_surface_heights(blocked_positions, highest_block)
+        push!(key, ((n_blocks - 1) % nb) + 1)
+        push!(key, ((wind_cursor - 1) % nw) + 1)
+        tkey = Tuple(key)
+        
+        # this is triggered when a cycle is detected so we can skip doing the rest
+        if haskey(cache, tkey)
+            cycle_start, start_highest = cache[tkey]
+            
+            # don't ask.
+            n_blocks -= 1
+            highest_block -= 2
+            blocks_required = n_blocks - cycle_start
+            height_gained = highest_block - start_highest
+            n_cycles = (n_block_max - cycle_start) ÷ blocks_required
+            return n_cycles * height_gained + heights[((n_block_max - cycle_start) % blocks_required) + cycle_start + 1] - 1
+        end
+        cache[tkey] = (n_blocks - 1, highest_block - 2)
 
         shift = highest_block + 3 - bottom_of_block + 1
         current_block = shift_block_down(current_block, -shift)
 
         while true
-            # @show wind_cursor, wind[((wind_cursor - 1) % nw) + 1]
             if wind[((wind_cursor - 1) % nw) + 1]
-                # if n_blocks <= 7
-                #     @show n_blocks, can_move_right(blocked_positions, current_block)
-                # end
                 if can_move_right(blocked_positions, current_block)
                     current_block = shift_block_right(current_block, 1)
                 end
             else
-                # if n_blocks <= 7
-                #     @show n_blocks, can_move_left(blocked_positions, current_block)
-                # end
                 if can_move_left(blocked_positions, current_block)
                     current_block = shift_block_right(current_block, -1)
                 end
@@ -103,86 +165,21 @@ function part_one(n_block_max)
             wind_cursor += 1
 
             if can_move_down(blocked_positions, current_block)
-                # if n_blocks <= 7
-                #     @show n_blocks, "moving down"
-                # end
                 current_block = shift_block_down(current_block, 1)
             else
                 break
             end
         end
 
-        # @show current_block
-
-        rows_to_check = []
         for i in eachindex(current_block)
             blocked_positions[current_block[i][1], current_block[i][2]] = 1
-            push!(rows_to_check, current_block[i][2])
             highest_block = max(highest_block, current_block[i][2])
-        end
-
-        for i in reverse(eachindex(rows_to_check))
-
-            # check whether we can erase everything below and continue
-            if all(blocked_positions[2:8, rows_to_check[i]])
-                new_bp = falses(9, 500)
-                new_bp[1, :] .= 1
-                new_bp[9, :] .= 1
-                new_bp[:, 1] .= 1
-                total_highest_block += rows_to_check[i] - 1
-                for j in rows_to_check[i] + 1:highest_block
-                    new_bp[:, j - (rows_to_check[i] + 1) + 2] = blocked_positions[:, j]
-                end
-                highest_block -= (rows_to_check[i] - 1)
-
-                blocked_positions = new_bp
-                break
-            end
         end
         n_blocks += 1
     end
 
-    # open("17.out", "w") do f
-    #     write(f, "+-------+\n")
-    #     for i in 1:49
-    #         write(f, "|")
-    #         for j in 2:8
-    #             if blocked_positions[j, 51 - i]
-    #                 write(f, "#")
-    #             else
-    #                 write(f, ".")
-    #             end
-    #         end
-    #         write(f, "|\n")
-    #     end
-    #     write(f, "+-------+\n")
-    # end
-
-    # println("+-------+")
-    # for i in 1:49
-    #     print("|")
-    #     for j in 2:8
-    #         if blocked_positions[j, 51 - i]
-    #             print("#")
-    #         else
-    #             print(".")
-    #         end
-    #     end
-    #     print("|")
-    #     println()
-    # end
-    # println("+-------+")
-
     return total_highest_block - 1 + highest_block
 end
 
-function part_two()
-    return nothing
-end
-
-println("PART ONE: ", part_one(2022))
-@time part_one(2022)
-@time part_one(20220)
-@time part_one(202200)
-@time part_one(1000000000000)
-# println("PART TWO: ", part_one(1000000000000))
+println("PART ONE: ", drop_rocks(2022))
+println("PART TWO: ", drop_rocks(1000000000000))
