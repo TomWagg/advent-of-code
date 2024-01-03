cc6de70686f8a129ef0a3b6cbce0b8f08300def1
Tom Wagg <tomjwagg@gmail.com>
2024-01-03 20:55:57 +0000

    better day 6

--- 2023/julia/6.jl
+++ 2023/julia/6.jl
@@ -1,45 +1,18 @@
-using BenchmarkTools
-
-function get_ways(time::Int, record::Int)
-    low, high = 0.5 * (time - sqrt(time^2 - 4 * record)), 0.5 * (time + sqrt(time^2 - 4 * record))
-    return Int(ceil(high - 1) - floor(low + 1) + 1)
-end
+sol(t, r, op) = 0.5 * op(t, √(t^2 - 4 * r))
+get_ways(time::Int, record::Int) = Int(ceil(sol(time, record, +) - 1) - floor(sol(time, record, -) + 1) + 1)
 
 function part_one()
-    times, records = Array{Int}(undef, 0), Array{Int}(undef, 0)
-    open("../inputs/6.txt", "r") do input
-        for line in eachline(input)
-            split_line = split(line, ":")
-            if split_line[1] == "Time"
-                times = parse.(Int, split(strip(split_line[2])))
-            else
-                records = parse.(Int, split(strip(split_line[2])))
-            end
-        end
-    end
+    lines = split.(split(read("../inputs/6.txt", String), '\n'))
+    data = parse.(Int, mapreduce(permutedims, vcat, lines)[:, 2:end])
+    times, records = data[1, :], data[2, :]
     return prod([get_ways(time, record) for (time, record) in zip(times, records)])
 end
 
 function part_two()
-    time, record = 0, 0
-    open("../inputs/6.txt", "r") do input
-        for line in eachline(input)
-            split_line = split(line, ":")
-            if split_line[1] == "Time"
-                time = parse.(Int, replace(split_line[2], " "=>""))
-            else
-                record = parse.(Int, replace(split_line[2], " "=>""))
-            end
-        end
-    end
+    time_str, record_str = split.(replace.(split(read("../inputs/6.txt", String), "\n"), " "=>""), ":")
+    time, record = parse(Int, time_str[2]), parse(Int, record_str[2])
     return get_ways(time, record)
 end
 
-function main()
-    println("PART ONE: ", part_one())
-    @btime part_one()
-    println("PART TWO: ", part_two())
-    @btime part_two()
-end
-
-main()
+println("PART ONE: ", part_one())
+println("PART TWO: ", part_two())
