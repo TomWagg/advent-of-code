1c28be8930cd78de556409bf061dd2a2770def36
Tom Wagg <tomjwagg@gmail.com>
2025-12-03 11:29:42 -0500

    day 3 2025

--- 2025/3.jl
+++ 2025/3.jl
@@ -1,18 +1,14 @@
-function recurse(bank::AbstractString, n_bat::Int, cache::Dict{Tuple{AbstractString, Int}, Int})
+function largest_joltage(bank::AbstractString, n_bat::Int, cache::Dict{Tuple{AbstractString, Int}, Int})
     if length(bank) == 1
         return parse(Int, bank), cache
     end
-    if n_bat == 0
-        return -1, cache
-    end
 
-    max_joltage = 0
     if (bank, n_bat) ∉ keys(cache)
         max_joltage = 0
         for i in 1:length(bank) - (n_bat - 1)
             joltage = parse(Int, bank[i]) * 10^(n_bat - 1)
             if n_bat > 1
-                more_joltage, cache = recurse(bank[i + 1:end], n_bat - 1, cache)
+                more_joltage, cache = largest_joltage(bank[i + 1:end], n_bat - 1, cache)
                 joltage += more_joltage
             end
             max_joltage = max(max_joltage, joltage)
@@ -24,21 +20,16 @@ function recurse(bank::AbstractString, n_bat::Int, cache::Dict{Tuple{AbstractStr
 end
 
 function main()
-    banks = [
-        bank for bank in eachsplit(read("inputs/3.txt", String), '\n')
-    ]
-
+    banks = [bank for bank in eachsplit(read("inputs/3.txt", String), '\n')]
     cache = Dict{Tuple{AbstractString, Int}, Int}()
 
-    part_one() = sum(recurse(bank, 2, cache)[1] for bank in banks)
-    part_two() = sum(recurse(bank, 12, cache)[1] for bank in banks)
+    part_one() = sum(largest_joltage(bank, 2, cache)[1] for bank in banks)
+    part_two() = sum(largest_joltage(bank, 12, cache)[1] for bank in banks)
 
     println("PART ONE: ", part_one())
     @time part_one()
     println("PART TWO: ", part_two())
     @time part_two()
-
-    # @show cache
 end
 
 main()
