a37f0fcf459b068a6998874fe092391b5ee3ac42
Tom Wagg <tomjwagg@gmail.com>
2025-12-03 11:16:29 -0500

    day 3 2025 (first draft)

---
+++ 2025/3.jl
@@ -0,0 +1,44 @@
+function recurse(bank::AbstractString, n_bat::Int, cache::Dict{Tuple{AbstractString, Int}, Int})
+    if length(bank) == 1
+        return parse(Int, bank), cache
+    end
+    if n_bat == 0
+        return -1, cache
+    end
+
+    max_joltage = 0
+    if (bank, n_bat) ∉ keys(cache)
+        max_joltage = 0
+        for i in 1:length(bank) - (n_bat - 1)
+            joltage = parse(Int, bank[i]) * 10^(n_bat - 1)
+            if n_bat > 1
+                more_joltage, cache = recurse(bank[i + 1:end], n_bat - 1, cache)
+                joltage += more_joltage
+            end
+            max_joltage = max(max_joltage, joltage)
+        end
+        cache[(bank, n_bat)] = max_joltage
+    end
+    
+    return cache[(bank, n_bat)], cache
+end
+
+function main()
+    banks = [
+        bank for bank in eachsplit(read("inputs/3.txt", String), '\n')
+    ]
+
+    cache = Dict{Tuple{AbstractString, Int}, Int}()
+
+    part_one() = sum(recurse(bank, 2, cache)[1] for bank in banks)
+    part_two() = sum(recurse(bank, 12, cache)[1] for bank in banks)
+
+    println("PART ONE: ", part_one())
+    @time part_one()
+    println("PART TWO: ", part_two())
+    @time part_two()
+
+    # @show cache
+end
+
+main()
